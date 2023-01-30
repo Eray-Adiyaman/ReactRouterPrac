083aa314efb5995de574e158d1d06ea92018dc5b
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-01-30 15:58:15 +0300

    ReactRouter setup,home,about pages,few styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter,Routes,Route,Link,NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-       1
-    </div>
+    <BrowserRouter>
+    <header>
+      <nav>
+        <h1>JobRouter</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+    </header>
+      <main>
+        <Routes>
+          <Route index /*or path="/" */  element={<Home />}/>
+          <Route path="/about"  element={<About />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
