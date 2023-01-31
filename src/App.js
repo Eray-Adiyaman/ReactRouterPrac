85b67f1035f9232806d2fd63f515d6cc09b1551c
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-01-31 17:27:32 +0300

    ReactRouter v6.4 Loader function for fecthing data

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 //layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
 import NotFound from "./pages/NotFound";
+import CareersLayout from "./layouts/CareersLayout";
 
 
 const router = createBrowserRouter(
@@ -20,9 +22,19 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index /*or path="/" */ element={<Home />} />
       <Route path="/about" element={<About />} />
+      
       <Route path="help" element={<HelpLayout />} >
         <Route path="faq" element={<Faq />}/>
         <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />} >
+        <Route 
+          index 
+          element={<Careers />}
+          loader={careersLoader} 
+        
+        />
       </Route>
 
       <Route path="*" element={<NotFound />} />
